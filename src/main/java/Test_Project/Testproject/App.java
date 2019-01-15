935ba210bdf2a502155156e26acc96fc011575e5
Joydip <45873508+Test20189@users.noreply.github.com>
2019-01-15 10:33:26 +0530

    Revert "merge code."

--- src/main/java/Test_Project/Testproject/App.java
+++ src/main/java/Test_Project/Testproject/App.java
@@ -7,6 +7,6 @@ public class App
 	@Test
     public void Testing()
     {
-        System.out.println( "I am the Best" );
+        System.out.println( "Joydip bhattacharjee is good boy" );
     }
 }
